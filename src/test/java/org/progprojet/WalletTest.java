c9a90072747e85607bee04c9e79c122eff224808
Mikolo <hei.mikolo.2@gmail.com>
2025-11-19 19:32:29 +0300

    fix: delete indesirable comment

--- src/test/java/org/progprojet/WalletTest.java
+++ src/test/java/org/progprojet/WalletTest.java
@@ -14,22 +14,22 @@ public class WalletTest {
         Wallet wallet = new Wallet("red", 5, 0.6, "Gucci");
         assertEquals(200, wallet.addMoney(200));
         assertEquals(220, wallet.addMoney(20));
-        assertEquals(220, wallet.checkMoney()); // vérifier le montant actuel
+        assertEquals(220, wallet.checkMoney());
     }
 
     @Test
     public void testGetMoney() {
         Wallet wallet = new Wallet("red", 5, 0.6, "Gucci");
         wallet.addMoney(100);
-        assertEquals(40, wallet.getMoney(60)); // récupère 60, reste 40
-        assertEquals(0, wallet.getMoney(50)); // ne peut pas récupérer plus que le reste
+        assertEquals(40, wallet.getMoney(60));
+        assertEquals(0, wallet.getMoney(50));
     }
 
     @Test
     public void testAddCard() {
         Wallet wallet = new Wallet("red", 3, 0.6, "Gucci");
-        assertTrue(wallet.addCard(2)); // ajoute 2 cartes
-        assertFalse(wallet.addCard(2)); // dépasse le porte-cartes
+        assertTrue(wallet.addCard(2));
+        assertFalse(wallet.addCard(2));
     }
 
     @Test
